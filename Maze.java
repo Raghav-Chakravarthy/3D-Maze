ace6ce65f15239ffab0ca1375edc8d990ade3706
daowstin <austinzou2603@gmail.com>
2023-02-14 12:30:30 -0500

    created variables/methods of maze

--- Maze.java
+++ Maze.java
@@ -1,2 +1,28 @@
 public class Maze {
+    private int mazeSize;
+    private Chamber[][][] chambers;
+    private int moves;
+    private Chamber rootChamber;
+    private Chamber solutionChamber;
+    Maze(){
+
+    }
+    public Chamber getChamberAt(Coordinate coord){
+
+    }
+    public Chamber[][] getLevel(int z){
+
+    }
+    public Chamber getRootChamber(){
+        return rootChamber;
+    }
+    public Chamber getSolutionChamber(){
+        return solutionChamber;
+    }
+    public int getMoves(){
+
+    }
+    public Chamber setChamber(Coordinate){
+
+    }
 }
