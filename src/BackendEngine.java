62cd5323925f4ad82910f456686bcc403a930eaa
dr-Jess <jessej.zong@gmail.com>
2023-02-14 12:39:20 -0500

    cleaned up incomplete classes for now

--- src/BackendEngine.java
+++
@@ -1,6 +0,0 @@
-public class BackendEngine {
-    public static void main(String[] args){
-        BackendEngine backend = new BackendEngine();
-        ViewEngine viewEngine = new ViewEngine();
-    }
-}
--- src/test.java
+++
@@ -1,4 +0,0 @@
-public class test{
-    // For each class, create a seperate method(s) within test.java
-    // Add to the unit test as you are developing
-}
