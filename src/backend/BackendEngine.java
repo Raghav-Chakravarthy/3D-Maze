d6c0781d3a679b4e3de175938cedde4ff6bbb328
Raghav Chakravarthy <vcraghav@yahoo.com>
2023-02-21 15:39:24 -0500

    Add Backend

--- src/backend/BackendEngine.java
+++ src/backend/BackendEngine.java
@@ -15,6 +15,7 @@ public class BackendEngine {
     public BackendEngine(){
         viewEngine = new ViewEngine();
     }
+    
     public static void main(String[] args){
         BackendEngine backend = new BackendEngine();
     }
--- src/backend/ViewEngine.java
+++ src/backend/ViewEngine.java
@@ -1,9 +1,8 @@
 package backend;
 import frontend.*;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import frontend.*;
+
 public class ViewEngine{
     private JFrame frame;
     private JPanel mainPanel, currentPanel, introDisplay, chamberDisplay, mapDisplay, endDisplay;
@@ -16,10 +15,10 @@ public class ViewEngine{
     public void setup(){
         frame = new JFrame();
         gameView = "mainview";
-        //introDisplay = new MenuView();
+        introDisplay = new Menu();
         mainPanel.setLayout(null);
 		mainPanel.setSize(frame.getSize());
-        //mainPanel.add(introDisplay);
+        mainPanel.add(introDisplay);
 		frame.add(mainPanel);
 		frame.setLayout(null);
 		frame.setVisible(true);
