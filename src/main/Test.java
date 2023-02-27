d6aff32ee5a6969b7d114ad35c6d353dbeda9426
Ramy Kaddouri <myramkadd@gmail.com>
2023-02-27 13:33:42 -0500

    Testing maze generation

--- src/main/Test.java
+++ src/main/Test.java
@@ -11,37 +11,20 @@ import backend.BackendEngine;
 import frontend.ChamberView;
 import maze.Chamber;
 import maze.Coordinate;
+import maze.Maze;
+import maze.MazeGenerator;
 import rendering.ColorUtils;
 import rendering.ImageWallArt;
 import utils.TextureManager;
 
 public class Test {
     public static void main(String[] args) {
-        Chamber chamber = new Chamber();
-        chamber.setCoordinates(new Coordinate(0, 0, 0));
-        chamber.setWallColor(ColorUtils.randomChamberColor());
-        chamber.setWallArt(ImageWallArt.generateWallArtFor(chamber, 3));
+        MazeGenerator generator = new MazeGenerator("easy");
+        Maze maze = generator.getMaze();
 
-        Chamber chamber2 = new Chamber();
-        chamber2.setCoordinates(new Coordinate(1,0,0));
-        chamber2.setWallColor(ColorUtils.randomChamberColor());
-        Chamber chamber3 = new Chamber();
-        chamber3.setCoordinates(new Coordinate(0,0,1));
-        chamber3.setWallColor(ColorUtils.randomChamberColor());
-        chamber.setChambers(new Chamber[] {
-            null, null, chamber3, null, null, chamber2
-        });
-        chamber3.setChambers(new Chamber[]{
-                null,null,null,chamber,null,null
-        });
-        chamber3.setWallArt(ImageWallArt.generateWallArtFor(chamber3, 2));
-        chamber2.setChambers(new Chamber[]{
-               null,null,null,null,chamber,null
-        });
-        chamber2.setWallArt(ImageWallArt.generateWallArtFor(chamber2, 2));
         BackendEngine backendEngine = new BackendEngine();
-        backendEngine.setChamber(chamber);
-        ChamberView chamberView = new ChamberView(chamber, backendEngine);
+        backendEngine.setChamber(maze.getChamberAt(new Coordinate(0, 0, 0)));
+        ChamberView chamberView = new ChamberView(maze.getChamberAt(new Coordinate(0, 0, 0)), backendEngine);
         chamberView.setPreferredSize(new Dimension(720,720));
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Test");
