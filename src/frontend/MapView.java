511e9c60913a0e5538d1c8ef59029a838f00ca8f
Raghav Chakravarthy <vcraghav@yahoo.com>
2023-02-27 14:07:09 -0500

    MapView.java class to avoid errors

--- src/frontend/MapView.java
+++ src/frontend/MapView.java
@@ -1,5 +1,13 @@
 package frontend;
 
-public class MapView {
+import javax.swing.JPanel;
+
+import backend.BackendEngine;
+import maze.Chamber;
+
+public class MapView extends JPanel{
+
+    public MapView(Chamber[][] level, BackendEngine backendEngine) {
+    }
     
 }
